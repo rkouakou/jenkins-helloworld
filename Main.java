089082c59f175719754e4d66d7048389ed3215a6
rkouakou <48698004+rkouakou@users.noreply.github.com>
2021-12-22 11:21:41 +0100

    Update Main.java
    OK

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Xavki Et Toute la famille RFK!");
+        System.out.println("Hello, Xavki Et Toute la famille RFK! Tout Noisy gère le barra");
     }
 }
 
